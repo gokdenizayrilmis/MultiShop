bfc70f85950b73292b31e037da70fee59fea26cb
Gokdeniz <ayrilmis.gokdeniz@gmail.com>
2026-03-27 22:08:08 +0300

    order controller yazıldı

---
+++ Services/Order/Core/MultiShop.Order.Application/Services/ServiceRegistiration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiShop.Order.Application.Services
+{
+    public static class ServiceRegistiration
+    {
+        public static void AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ServiceRegistiration).Assembly));
+        }
+    }
+}
