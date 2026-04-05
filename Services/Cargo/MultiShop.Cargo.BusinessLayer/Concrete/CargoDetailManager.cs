a0ca46c8c0f40c792cb0cbecab2bdaa81b8d2507
Gokdeniz <ayrilmis.gokdeniz@gmail.com>
2026-04-05 11:15:52 +0300

    added changes

---
+++ Services/Cargo/MultiShop.Cargo.BusinessLayer/Concrete/CargoDetailManager.cs
@@ -0,0 +1,43 @@
+﻿using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.DataAccessLayer.Abstract;
+using MultiShop.Cargo.EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiShop.Cargo.BusinessLayer.Concrete
+{
+    public class CargoDetailManager : ICargoDetailService
+    {
+        private readonly ICargoDetailDal cargoDetail;
+        public CargoDetailManager(ICargoDetailDal cargoDetail)
+        {
+            this.cargoDetail = cargoDetail;
+        }
+
+        public void TDelete(int id)
+        {
+            cargoDetail.Delete(id);
+        }
+
+        public IEnumerable<CargoDetail> TGetAll()
+        {
+            return cargoDetail.GetAll();
+        }
+
+        public CargoDetail TGetById(int id)
+        {
+            return cargoDetail.GetById(id);
+        }
+
+        public void TInsert(CargoDetail entity)
+        {
+            cargoDetail.Insert(entity);
+        }
+
+        public void TUpdate(CargoDetail entity)
+        {
+            cargoDetail.Update(entity);
+        }
+    }
+}
---
+++ Services/Cargo/MultiShop.Cargo.WebApi/Controllers/CargoCompanyController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.EntityLayer.Concreate;
+
+namespace MultiShop.Cargo.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CargoCompanyController : ControllerBase
+    {
+        private readonly ICargoCompanyService _cargoCompanyService;
+
+        public CargoCompanyController(ICargoCompanyService cargoCompanyService)
+        {
+            _cargoCompanyService = cargoCompanyService;
+        }
+
+        [HttpGet]
+        public IActionResult CargoCompanyList()
+        {
+            var values = _cargoCompanyService.TGetAll();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCargoCompany()
+        {
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult RemoveCargoCompany(int id)
+        {
+            _cargoCompanyService.TDelete(id);
+            return Ok("Kargo Şirketi Başarıyla Silindi!");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCargoCompanyById(int id)
+        {
+            var value = _cargoCompanyService.TGetById(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCargoCompany(CargoCompany cargoCompany)
+        {
+            _cargoCompanyService.TUpdate(cargoCompany);
+            return Ok("Kargo Şirketi Başarıyla Güncellendi!");
+        }
+
+    }
+}
